1db73e8a9871a02465758d0e87fafec5d9e3cd7f
Declan Balla <declanab@gmail.com>
2023-03-16 17:24:29 -0700

    Adding the API

---
+++ stacks/ApiStack.js
@@ -0,0 +1,29 @@
+import { Api, use } from "sst/constructs";
+import { StorageStack } from "./StorageStack";
+
+export function ApiStack({ stack, app }) {
+  const { table } = use(StorageStack);
+
+  const api = new Api(stack, "Api", {
+    defaults: {
+      function: {
+        bind: [table]
+      }, 
+    },
+    routes: {
+      "GET /notes/{id}": "packages/functions/src/get.main",
+      "PUT /notes/{id}": "packages/functions/src/update.main",
+      "DELETE /notes/{id}": "packages/functions/src/delete.main",
+      "GET /notes": "packages/functions/src/list.main",
+      "POST /notes": "packages/functions/src/create.main",
+    },
+  });
+
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
+
+  return {
+    api,
+  }
+}
